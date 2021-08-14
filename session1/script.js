6bb53098e3aebf20ced2c87062f6acf5b633ae46
sarvjit <sarvjitlala@gmail.com>
2021-08-15 00:47:53 +0530

    change dom traversal

--- session1/script.js
+++ session1/script.js
@@ -9,15 +9,15 @@
 // }
 
 
-const grandparent =document.getElementById("grandparent-id")
-// const parent=Array.from(document.getElementsByClassName("parent"))
-const child=Array.from(document.getElementsByClassName("child"))
-// parent.forEach(changeColor)
-changeColor(child[1])
-console.log(parent)
-function changeColor(element){
- element.style.background="#333"
-}
+// const grandparent =document.getElementById("grandparent-id")
+// // const parent=Array.from(document.getElementsByClassName("parent"))
+// const child=Array.from(document.getElementsByClassName("child"))
+// // parent.forEach(changeColor)
+// changeColor(child[1])
+// console.log(parent)
+// function changeColor(element){
+//  element.style.background="#333"
+// }
 
 
 // const grandparent = document.querySelector("#grandparent-id")
@@ -32,3 +32,93 @@ function changeColor(element){
 // function changeColor(element){
 //        element.style.background="#333"
 //             }
+
+
+// const grandparent = document.querySelector(".grandparent")
+// const parent = Array.from(grandparent.children)
+// parent.forEach(changeColor)
+// function changeColor(element){
+//         element.style.background="#333"
+//             }
+
+
+
+//  const grandparent = document.querySelector(".grandparent")
+// const parent = Array.from(grandparent.children)
+// const parentOne=parent[0]
+// const children=parentOne.children
+// changeColor(children[0])
+// function changeColor(element){
+//              element.style.background="#333"
+//             }
+
+
+
+// const grandparent = document.querySelector(".grandparent")
+// const childOne = grandparent.querySelector(".child")
+// changeColor(childOne)
+// function changeColor(element){
+//              element.style.background="#333"
+//             }
+
+
+// const grandparent = document.querySelector(".grandparent")
+// const children = grandparent.querySelectorAll(".child")
+// children.forEach(changeColor)
+// function changeColor(element){
+//              element.style.background="#333"
+//             }
+
+
+
+// const childOne = document.querySelector("#child-one")
+// changeColor(childOne)
+// function changeColor(element){
+//              element.style.background="#333"
+//             }
+
+
+// const childOne = document.querySelector("#child-one")
+// const parent=childOne.parentElement
+// changeColor(parent)
+// function changeColor(element){
+//              element.style.background="#333"
+//             }
+
+
+
+// const childOne = document.querySelector("#child-one")
+// const parent=childOne.parentElement
+// const grandparent=parent.parentElement
+// changeColor(grandparent)
+// function changeColor(element){
+//              element.style.background="#333"
+//             }
+
+
+
+
+// const childOne = document.querySelector("#child-one")
+// const grandparent=childOne.closest(".grandparent")
+// changeColor(grandparent)
+// function changeColor(element){
+//              element.style.background="#333"
+//             }
+
+
+
+// const childOne = document.querySelector("#child-one")
+// const childTwo=childOne.nextElementSibling
+// changeColor(childTwo)
+// function changeColor(element){
+//              element.style.background="#333"
+//             }
+
+
+
+const childOne = document.querySelector("#child-one")
+const childTwo=childOne.nextElementSibling
+changeColor(childTwo.previousElementSibling)
+function changeColor(element){
+             element.style.background="#333"
+            }
